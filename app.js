866b03693c1aac62953f88a07104ca07098dac1d
girrajKD <girraj.khedmah@gmail.com>
2026-04-17 19:13:46 +0400

    image file upload api added

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const errorHandler = require("./middleware/errorMiddleware");
+const path = require("path");
 
 connectDB();
 app.use(express.json());
 app.use("/", userRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(errorHandler);
 
 app.listen(3000, () => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,7 @@ const {
   loginUser,
 } = require("../controllers/userController");
 const protect = require("../middleware/authMiddleware");
+const upload = require("../middleware/uploadMiddleware");
 
 // Register
 router.post("/user/signup", createUser);
@@ -18,5 +19,16 @@ router.post("/user/login", loginUser);
 // GET  user by id
 router.get("/user/profile/:id", protect, getUserById);
 router.get("/users", protect, getUsers);
+router.post(
+  "/user/upload-profile-pic",
+  protect,
+  upload.single("profile-image"),
+  (req, res) => {
+    res.json({
+      message: "Image uploaded",
+      file: `/uploads/${req.file.filename}`,
+    });
+  },
+);
 
 module.exports = router;
